79890cc30b400c62f0cf8a79198abb3e9188d0ab
leocarlos-dias <leocsdias@hotmail.com>
2023-06-28 22:02:04 +0000

    feat: add kafka producer

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,23 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { InputOrderDto } from './orders.dto';
 import { OrderStatus } from '../../../node_modules/.prisma/client/orders';
+import { ClientKafka } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+import { OrderEventService } from './order-event.service';
 
 @Injectable()
 export class OrdersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private orderEventService: OrderEventService,
+  ) {}
 
   async getAll() {
     return await this.prisma.order.findMany();
   }
 
   async create({ price, customerId }: InputOrderDto) {
-    return await this.prisma.order.create({
+    const order = await this.prisma.order.create({
       data: {
         price,
         customer_id: customerId,
         status: OrderStatus.PROCESSING,
       },
     });
+    this.orderEventService.emitOrderCreatedEvent(order);
+    return order;
   }
 }
